38f3d723ba032fbf5dc1bb255f0fecf55c825e97
Matěj Trakal <mtrakal@gmail.com>
2017-03-08 16:08:04 +0100

    Issue 234873
    https://code.google.com/p/android/issues/detail?id=234873

---
+++ app/src/main/java/cz/mtrakal/issue234873/MainActivity.java
@@ -0,0 +1,15 @@
+package cz.mtrakal.issue234873;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ((TextView) findViewById(R.id.date)).setText(BuildConfig.BUILD_DATE);
+    }
+}
